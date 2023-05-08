5b78dccb80e90ee68f94e2727000791395051571
AndreCodeMuseum <andregit@skiff.com>
2023-05-08 07:27:49 -0300

    A structure of condition was created to tell if the student has passed or not the exam

--- StudentAverage.py
+++ StudentAverage.py
@@ -1,4 +1,4 @@
-# Read to grades of a student exam and tell its average 
+# Read to grades of a student exam and tell its average and if his grade is equal or higher than 6 he has passed the exam
 
 examOne = float(input("1º Exam: "))
 examTwo = float(input("2º Exam: "))
@@ -8,6 +8,13 @@ print()
 
 averageGrade = (examOne + examTwo) / 2
 
+
+if averageGrade >= 6:
+    print("Well done!, You've passed the exam")
+else:
+    print("Sorry, you've failed the exam")
+
+# Output
 
 print("Average:", averageGrade)
 
